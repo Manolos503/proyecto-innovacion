055283e4ad4b2428c4320e74d7b1cf69ae0e3f9f
neftali03 <neftalihrramos03@gmail.com>
2025-05-16 20:31:35 -0600

    Add new index, add styles and fix toggle color mode

---
+++ resources/views/common/_navbar.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg bg-primary py-1">
+  <div class="container-fluid ps-0">
+    <div class="d-flex flex-row fw-bold">
+      <button id="sidebar-toggle-btn"
+              type="button"
+              class="btn border-0 text-white"
+              data-bs-toggle="collapse"
+              data-bs-target="#sidebar"
+              aria-expanded="true"
+              aria-controls="sidebar">
+        <i class="bi bi-list"></i>
+      </button>
+      <a class="navbar-brand ms-2 {{ config('app.debug') ? 'text-light' : 'text-white' }}" href="{{ route('index') }}">
+        O.V.
+      </a>
+    </div>
+
+    <button id="toggle-color-mode" class="btn text-white btn-sm me-2">
+      <i class="bi bi-moon-fill"></i>
+    </button>
+
+    <div class="dropdown me-2">
+      <button class="btn text-white btn-sm dropdown-toggle py-1 border-0"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false">
+      </button>
+
+      <ul class="dropdown-menu dropdown-menu-end">
+        <li>
+            <button class="dropdown-item">Cerrar sesión</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ resources/views/common/_sidebar.blade.php
@@ -0,0 +1,29 @@
+<div id="sidebar" class="collapse show">
+  <div id="sidebar-nav" class="accordion accordion-flush dx-w-sidebar-md">
+    <div class="accordion-item">
+    <div class="accordion-header">
+        <button
+        class="accordion-button py-2"
+        type="button" data-bs-toggle="collapse"
+        data-bs-target="#collapse"
+        aria-expanded=""
+        aria-controls="collapse">
+        </button>
+    </div>
+
+    <div id="collapse"
+        data-bs-parent="sidebar-nav"
+        class="accordion-collapse collapse">
+        <div class="accordion-body bg-body py-0 ps-3">
+        <ul class="list-group list-group-flush">
+                <li class="list-group-item border-0">
+                <a href=""
+                    class="text-decoration-none">
+                </a>
+                </li>
+        </ul>
+        </div>
+    </div>
+    </div>
+  </div>
+</div>
